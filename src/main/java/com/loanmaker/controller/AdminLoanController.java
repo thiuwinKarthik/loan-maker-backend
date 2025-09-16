2a6ed16e387c9cbefe539cdb84cb316ee598fe7f
thiuwinKarthik <thiruwinkarthik4@gmail.com>
2025-09-16 03:31:30 -0700

    Update AdminLoanController.java

--- src/main/java/com/loanmaker/controller/AdminLoanController.java
+++ src/main/java/com/loanmaker/controller/AdminLoanController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin/loans")
-@CrossOrigin(origins = "https://timely-tanuki-loan-maker.netlify.app")
+@CrossOrigin(origins = "https://loan-maker.netlify.app")
 public class AdminLoanController {
 
     @Autowired
